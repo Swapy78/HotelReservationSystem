466dabb92359a04596427e93a36f14d623be7a6e
Swapy78 <swapnilbabanmane@gmail.com>
2023-07-17 16:57:08 +0530

    Added java program to find cheapest hotel

---
+++ src/Hotel.java
@@ -0,0 +1,45 @@
+
+package demo;
+
+import java.util.*;
+
+class Hotel {
+
+	private static final String Lakewood = null;
+	private static final String Bridgewood = null;
+	private static final String Ridgewood = null;
+
+	public static void main(String[] args) {
+		
+		
+		Hotel hotel = new Hotel();
+		hotel.add(Lakewood,110,3);
+		hotel.add(Bridgewood,160,4);
+		hotel.add(Ridgewood,220,5);
+		
+		System.out.println("Available hotels are :" +hotel);
+		
+
+	
+
+	
+	
+
+	
+
+		
+
+	}
+
+		
+		
+		
+
+	
+
+	private void add(String lakewood2, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
